c23da6748bf3ba1f181c3bfa36e49eeda89818f6
Dillan Prince <dillan.prince@charthop.com>
2022-01-24 11:12:59 -0600

    update how current hour is determined

--- src/listeners/index.js
+++ src/listeners/index.js
@@ -1,47 +1,33 @@
-import { COMMAND_PREFIX } from '../common/constants.js';
+import { COMMAND_PREFIX } from "../common/constants.js";
 let running = false;
 
-export const onMessage = (message) => {
-    if (
-        running ||
-        !message.content.startsWith(COMMAND_PREFIX) ||
-        message.author.bot
-    ) {
-        return;
+export const onMessage = message => {
+  if (running || !message.content.startsWith(COMMAND_PREFIX) || message.author.bot) {
+    return;
+  }
+
+  const args = message.content.slice(COMMAND_PREFIX.length).trim().split(/ +/);
+
+  const command = args.shift().toLowerCase();
+
+  if (command !== "start") {
+    return;
+  }
+
+  running = true;
+
+  setInterval(() => {
+    const currentHour = new Date(
+      new Date().toLocaleString("en-US", {
+        timeZone: "America/Chicago",
+      })
+    ).getHours();
+
+    console.log(`hours: ${currentHour}`);
+
+    if (8 <= currentHour && currentHour <= 16) {
+      console.log("Sending check-in message.");
+      message.channel.send("<@229105932993429504> how are you?");
     }
-
-    const args = message.content
-        .slice(COMMAND_PREFIX.length)
-        .trim()
-        .split(/ +/);
-
-    const command = args.shift().toLowerCase();
-
-    if (command !== 'start') {
-        return;
-    }
-
-    running = true;
-
-    setInterval(() => {
-        const datetime = new Date().toLocaleString('en-US', {
-            timeZone: 'America/Chicago'
-        });
-
-        const [date, time] = datetime.split(',');
-        const [hours, minutes, s] = time.split(':');
-        const [seconds, period] = s.split(' ');
-
-        const hoursInt = parseInt(hours);
-
-        console.log(`hoursInt: ${hoursInt} ; period: ${period}`);
-
-        if (
-            (hoursInt >= 8 && period === 'AM') ||
-            (hoursInt <= 4 && period === 'PM')
-        ) {
-            console.log('Sending check-in message.');
-            message.channel.send('<@229105932993429504> how are you?');
-        }
-    }, 1000 * 60 * 60);
+  }, 1000 * 60 * 60);
 };
